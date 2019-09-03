fc33905ceddaea01e778bac3c8d30a64b4d3e515
Jisung Yoo <jyoo@ezcaretech.com>
2019-09-03 18:15:19 +0900

    Fix working properly after connecting ssh

--- upgrade.sh
+++ upgrade.sh
@@ -14,18 +14,11 @@ PORT='YOUR PORT'
 # REMOTE LOCATION
 REMOTE_LOCATION='~'
 
-# EXIST => next step
-
-# NOT EXIST => GOTO FIND NAME
-
 # COPY LOCAL TO REMOTE
 echo scp -P $PORT $FILE_NAME $ID@$ADDRESS:$REMOTE_LOCATION
 scp -P $PORT $FILE_NAME $ID@$ADDRESS:$REMOTE_LOCATION
 
 # UPGRADE
-echo mtd-write2 $FILE_NAME linux && reboot
-mtd-write2 $FILE_NAME linux && reboot
+echo ssh $ID@$ADDRESS -p $PORT 'mtd-write2 $FILE_NAME linux && reboot'
+ssh $ID@$ADDRESS -p $PORT 'mtd-write2 $FILE_NAME linux && reboot'
 
-# ssh
-echo ssh $ID@$ADDRESS -p $PORT
-ssh $ID@$ADDRESS -p $PORT
